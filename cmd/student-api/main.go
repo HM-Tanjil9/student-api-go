6f6c291ea9147ed3f415b8fe6929e74ec7325de6
HM-Tanjil9 <rtajil0@gmail.com>
2026-04-22 18:33:15 +0600

    add request validation

--- cmd/student-api/main.go
+++ cmd/student-api/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/HM-Tanjil9/student-api-go/internal/config"
+	"github.com/HM-Tanjil9/student-api-go/internal/http/handlers/student"
 )
 
 func main() {
@@ -20,9 +21,7 @@ func main() {
 	// database setup
 	// setup router
 	router := http.NewServeMux()
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to students api"))
-	})
+	router.HandleFunc("POST /api/students", student.New())
 	// setup server
 	server := http.Server{
 		Addr:    cfg.Addr,
---
+++ internal/http/handlers/student/student.go
@@ -0,0 +1,36 @@
+package student
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/HM-Tanjil9/student-api-go/internal/types"
+	"github.com/HM-Tanjil9/student-api-go/internal/utils/response"
+	"github.com/go-playground/validator/v10"
+)
+
+func New() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var student types.Student
+		err := json.NewDecoder(r.Body).Decode(&student)
+		if errors.Is(err, io.EOF) {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(fmt.Errorf("empty body")))
+			return
+		}
+		if err != nil {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+		// Request validation
+		if err := validator.New().Struct(student); err != nil {
+			validateErrors := err.(validator.ValidationErrors)
+			response.WriteJson(w, http.StatusBadRequest, response.ValidationError(validateErrors))
+			return
+		}
+
+		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "OK"})
+	}
+}
